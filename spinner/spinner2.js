c6e5372364f20b8359e021a5e7a1414d9956c13b
Alex <alextheprogrammer21@gmail.com>
2020-02-11 20:58:11 +0000

    worked with @nrpope

---
+++ spinner/spinner2.js
@@ -0,0 +1,6 @@
+let arr = ["\r|   ", "\r/   ", "\r-   ", "\r\\   ", "\r|   ", "\r/   ", "\r-   ", "\r\\   ", "\r|   "]
+for (let i = 0; i < 9; i++) { setTimeout(() => { process.stdout.write(arr[i]); }, 200 + (i*300)); }
+
+
+
+
