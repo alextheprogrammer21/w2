44fe8f54074d6471a511426b498d69ff5536c7b4
Alex <alextheprogrammer21@gmail.com>
2020-02-11 19:42:44 +0000

    Week two commit

---
+++ d1/CaesarCipher/caesarCipher.js
@@ -0,0 +1,7 @@
+//Create an array of alphabet and convert the string to numbers, then return an array with the indexes of the crypted array 
+
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+let numericalArray = [];
+
+let letterPosition = alphabet.indexOf(letter)+1; 
